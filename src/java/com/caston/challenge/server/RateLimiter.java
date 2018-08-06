310c09cdd41859923a33b6164356ea0e4081af8e
Nick Caston <ntcaston@gmail.com>
2018-08-06 21:53:18 +1000

    clarify contract of rate limiter interface

--- src/java/com/caston/challenge/server/RateLimiter.java
+++ src/java/com/caston/challenge/server/RateLimiter.java
@@ -13,8 +13,9 @@ public interface RateLimiter<T> {
   /**
    * Determines if an exchange should be rate limited. If the exchange is to be
    * rate limited, this method will return true and perform any necessary
-   * updates to the exchange. If the exchange should not be rate limited, this
-   * method will return false and callers should handle this exchange normally.
+   * updates to the exchange to complete it. If the exchange should not be rate
+   * limited, this method will return false and callers should handle this
+   * exchange normally.
    *
    * This method must be thread-safe.
    *
